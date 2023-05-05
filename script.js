d8bd0b57b04627ed6f9ad9af416841a04636f290
Nagisa-Kanagi <anteiku27@gmail.com>
2023-05-05 16:31:15 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,255 @@
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
+
+const img = document.getElementById("img1");
+function enlargeImg() {
+  img.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img3.style = "scale(1)";
+  img4.style = "scale(1)";
+  img5.style = "scale(1)";
+  img6.style = "scale(1)";
+  img7.style = "scale(1)";
+  img8.style = "scale(1)";
+  img9.style = "scale(1)";
+  img10.style = "scale(1)";
+  img11.style = "scale(1)";
+  img12.style = "scale(1)";
+  img.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg) {
+    var img = document.getElementById("img1");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img2 = document.getElementById("img2");
+function enlargeImg2() {
+  img2.style.transform = "scale(1.5)";
+  img.style = "scale(1)";
+  img3.style = "scale(1)";
+  img4.style = "scale(1)";
+  img5.style = "scale(1)";
+  img6.style = "scale(1)";
+  img7.style = "scale(1)";
+  img8.style = "scale(1)";
+  img9.style = "scale(1)";
+  img10.style = "scale(1)";
+  img11.style = "scale(1)";
+  img12.style = "scale(1)";
+  img2.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg2) {
+    var img = document.getElementById("img2");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img3 = document.getElementById("img3");
+function enlargeImg3() {
+  img3.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img.style = "scale(1)";
+  img4.style = "scale(1)";
+  img5.style = "scale(1)";
+  img6.style = "scale(1)";
+  img7.style = "scale(1)";
+  img8.style = "scale(1)";
+  img9.style = "scale(1)";
+  img10.style = "scale(1)";
+  img11.style = "scale(1)";
+  img12.style = "scale(1)";
+  img3.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg3) {
+    var img = document.getElementById("img3");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img4 = document.getElementById("img4");
+function enlargeImg4() {
+  img4.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img3.style = "scale(1)";
+  img.style = "scale(1)";
+  img5.style = "scale(1)";
+  img6.style = "scale(1)";
+  img7.style = "scale(1)";
+  img8.style = "scale(1)";
+  img9.style = "scale(1)";
+  img10.style = "scale(1)";
+  img11.style = "scale(1)";
+  img12.style = "scale(1)";
+  img4.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg4) {
+    var img = document.getElementById("img4");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img5 = document.getElementById("img5");
+function enlargeImg5() {
+  img5.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img3.style = "scale(1)";
+  img4.style = "scale(1)";
+  img.style = "scale(1)";
+  img6.style = "scale(1)";
+  img7.style = "scale(1)";
+  img8.style = "scale(1)";
+  img9.style = "scale(1)";
+  img10.style = "scale(1)";
+  img11.style = "scale(1)";
+  img12.style = "scale(1)";
+  img5.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg5) {
+    var img = document.getElementById("img5");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img6 = document.getElementById("img6");
+function enlargeImg6() {
+  img6.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img3.style = "scale(1)";
+  img4.style = "scale(1)";
+  img5.style = "scale(1)";
+  img.style = "scale(1)";
+  img7.style = "scale(1)";
+  img8.style = "scale(1)";
+  img9.style = "scale(1)";
+  img10.style = "scale(1)";
+  img11.style = "scale(1)";
+  img12.style = "scale(1)";
+  img6.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg6) {
+    var img = document.getElementById("img6");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img7 = document.getElementById("img7");
+function enlargeImg7() {
+  img7.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img3.style = "scale(1)";
+  img4.style = "scale(1)";
+  img5.style = "scale(1)";
+  img6.style = "scale(1)";
+  img.style = "scale(1)";
+  img8.style = "scale(1)";
+  img9.style = "scale(1)";
+  img10.style = "scale(1)";
+  img11.style = "scale(1)";
+  img12.style = "scale(1)";
+  img7.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg7) {
+    var img = document.getElementById("img7");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img8 = document.getElementById("img8");
+function enlargeImg8() {
+  img8.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img3.style = "scale(1)";
+  img4.style = "scale(1)";
+  img5.style = "scale(1)";
+  img6.style = "scale(1)";
+  img7.style = "scale(1)";
+  img.style = "scale(1)";
+  img9.style = "scale(1)";
+  img10.style = "scale(1)";
+  img11.style = "scale(1)";
+  img12.style = "scale(1)";
+  img8.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg8) {
+    var img = document.getElementById("img8");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img9 = document.getElementById("img9");
+function enlargeImg9() {
+  img9.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img3.style = "scale(1)";
+  img4.style = "scale(1)";
+  img5.style = "scale(1)";
+  img6.style = "scale(1)";
+  img7.style = "scale(1)";
+  img8.style = "scale(1)";
+  img.style = "scale(1)";
+  img10.style = "scale(1)";
+  img11.style = "scale(1)";
+  img12.style = "scale(1)";
+  img9.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg) {
+    var img = document.getElementById("img9");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img10 = document.getElementById("img10");
+function enlargeImg10() {
+  img10.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img3.style = "scale(1)";
+  img4.style = "scale(1)";
+  img5.style = "scale(1)";
+  img6.style = "scale(1)";
+  img7.style = "scale(1)";
+  img8.style = "scale(1)";
+  img9.style = "scale(1)";
+  img.style = "scale(1)";
+  img11.style = "scale(1)";
+  img12.style = "scale(1)";
+  img10.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg) {
+    var img = document.getElementById("img10");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img11 = document.getElementById("img11");
+function enlargeImg11() {
+  img11.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img3.style = "scale(1)";
+  img4.style = "scale(1)";
+  img5.style = "scale(1)";
+  img6.style = "scale(1)";
+  img7.style = "scale(1)";
+  img8.style = "scale(1)";
+  img9.style = "scale(1)";
+  img10.style = "scale(1)";
+  img.style = "scale(1)";
+  img12.style = "scale(1)";
+  img11.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg) {
+    var img = document.getElementById("img11");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
+const img12 = document.getElementById("img12");
+function enlargeImg12() {
+  img12.style.transform = "scale(1.5)";
+  img2.style = "scale(1)";
+  img3.style = "scale(1)";
+  img4.style = "scale(1)";
+  img5.style = "scale(1)";
+  img6.style = "scale(1)";
+  img7.style = "scale(1)";
+  img8.style = "scale(1)";
+  img9.style = "scale(1)";
+  img10.style = "scale(1)";
+  img11.style = "scale(1)";
+  img.style = "scale(1)";
+  img12.style.transitionDuration = "1s";
+  setTimeout(function (enlargeImg12) {
+    var img = document.getElementById("img12");
+    img.style.transform = "scale(1)";
+  }, 5000);
+}
